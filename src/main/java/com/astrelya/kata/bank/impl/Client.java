588321f357f221f064dda1ac57e0cc2e37bd1093
odebbabi2 <debbabi.oumaima1@gmail.com>
2025-06-13 10:24:47 +0200

    fix(internal): Firstcommit ajouter et identifier la classe Client

---
+++ src/main/java/com/astrelya/kata/bank/impl/Client.java
@@ -0,0 +1,39 @@
+package com.astrelya.kata.bank.impl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.astrelya.kata.bank.IClient;
+
+public class Client implements IClient {
+	private final  String email;
+	private final Collection<Object> productList = new ArrayList<>();
+	private BigDecimal monthyBalance = java.math.BigDecimal.ZERO;
+
+	public Client(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public Collection<Object> getProductList() {
+		return productList;
+	}
+
+	@Override
+	public BigDecimal getMonthlyBalance() {
+		return monthyBalance;
+	}
+
+	@Override
+	public void addProduct(String productType, Double amount) {
+		productList.add(productType + "-" + amount);
+
+	}
+
+}
